6b3f9a555a0f19f9518274089b2998dbbe687240
sarahlein <sarah.meitz@stud.fh-campuswien.ac.at>
2019-05-18 21:19:45 +0200

    assignment 3 finished

--- assignment3.py
+++ assignment3.py
@@ -2,8 +2,10 @@
 
 import vcf
 import httplib2
+import os
+import subprocess
 
-__author__ = 'XXX'
+__author__ = "Sarah Meitz"
 
 
 ##
@@ -23,10 +25,14 @@ class Assignment3:
     def __init__(self):
         ## Check if pyvcf is installed
         print("PyVCF version: %s" % vcf.VERSION)
-        
-        ## Call annotate_vcf_file here
-        self.vcf_path = ""  # TODO
 
+        ## Call annotate_vcf_file here
+        self.vcf_path = os.path.join(os.getcwd(), "chr16.vcf")  # TODO
+
+        if not os.path.isfile(self.vcf_path):
+            subprocess.call(["wget", "http://hmd.ait.ac.at/medgen2019/chr16.vcf"])
+
+        self.annotations_dict = self.annotate_vcf_file()
     def annotate_vcf_file(self):
         '''
         - Annotate the VCF file using the following example code (for 1 variant)
@@ -34,12 +40,13 @@ class Assignment3:
         - Store the result in a data structure
         :return:
         '''    
-        print("TODO")
-                
+
         ##
         ## Example loop
         ##
-        
+
+
+
         ## Build the connection
         h = httplib2.Http()
         headers = {'content-type': 'application/x-www-form-urlencoded'}
@@ -57,16 +64,35 @@ class Assignment3:
         params = 'ids=' + ",".join(params_pos) + '&hg38=true'
         
         ## Perform annotation
-        res, con = h.request('http://myvariant.info/v1/variant', 'POST', params, headers=headers)
-        annotation_result = con.decode('utf-8')
+        #res, con = h.request('http://myvariant.info/v1/variant', 'POST', params, headers=headers)
+        #annotation_result = con.decode('utf-8')
+
+
+        import myvariant
+        mv = myvariant.MyVariantInfo()
+        ann = mv.getvariants(params)
+
+        annotation_dict = []
+        count = 0
+        for x in ann:
+            try:
+                if x['notfound']:
+                    pass
+            except:
+                annotation_dict.append(x)
+                count += 1
+        #print(count) # 24 gefunden
+
+
         
         ## TODO now do something with the 'annotation_result'
-        
+
+
         ##
         ## End example code
         ##
         
-        return None  ## return the data structure here
+        return annotation_dict  ## return the data structure here
     
     
     def get_list_of_genes(self):
@@ -74,15 +100,32 @@ class Assignment3:
         Print the name of genes in the annotation data set
         :return:
         '''
-        print("TODO")
-    
+
+        genes = []
+
+        for hit in self.annotations_dict:
+            try:
+                if hit['cadd']['gene']:
+                    # print(hit['cadd']['gene']['genename'])
+                    genes.append(hit['cadd']['gene']['genename'])
+            except:
+                pass
+        print("Genes in annotation data set:\t ", genes)
+
     
     def get_num_variants_modifier(self):
         '''
         Print the number of variants with putative_impact "MODIFIER"
         :return:
         '''
-        print("TODO")
+        count = 0
+        for hit in self.annotations_dict:
+            if 'snpeff' in hit:
+                key, value = "putative_impact", "MODIFIER"
+                if key in hit['snpeff']['ann'] and value == hit['snpeff']['ann']['putative_impact']:
+                    count += 1
+
+        print("Number of variants with putative_impact 'MODIFIER':\t ", count)
         
     
     def get_num_variants_with_mutationtaster_annotation(self):
@@ -90,15 +133,31 @@ class Assignment3:
         Print the number of variants with a 'mutationtaster' annotation
         :return:
         '''
-        print("TODO")
-        
+        count = 0
+        for hit in self.annotations_dict:
+            try:
+                if hit['dbnsfp']['mutationtaster']:
+                    count += 1
+            except:
+                pass
+        print("Number of variants with a 'mutationtaster' annotation:\t ", count)
     
     def get_num_variants_non_synonymous(self):
         '''
         Print the number of variants with 'consequence' 'NON_SYNONYMOUS'
         :return:
         '''
-        print("TODO")
+        count = 0
+        for hit in self.annotations_dict:
+            try:
+                if 'cadd' in hit:
+                    key, value = "consequence", "NON_SYNONYMOUS"
+                    if key in hit['cadd'] and value == hit['cadd']['consequence']:
+                        count += 1
+            except:
+                pass
+
+        print("Number of variants with 'consequence' 'NON_SYNONYMOUS':\t ", count)
         
     
     def view_vcf_in_browser(self):
@@ -109,12 +168,15 @@ class Assignment3:
         '''
    
         ## Document the final URL here
-        print("TODO")
+        print("View vcf in browser: https://vcf.iobio.io/?species=Human&build=GRCh38")
             
     
     def print_summary(self):
-        self.annotate_vcf_file()
-        print("Print all results here")
+        self.get_list_of_genes()
+        self.get_num_variants_modifier()
+        self.get_num_variants_with_mutationtaster_annotation()
+        self.get_num_variants_non_synonymous()
+        self.view_vcf_in_browser()
     
     
 def main():
